cef37838877d4ab64cba51dfb5adc55441902898
victor_xu <victor_xu@auer.com.tw>
2025-02-22 08:31:41 +0800

    W01-P3: implement divide button

--- demo/w01_dom_43/js/w01_43.js
+++ demo/w01_dom_43/js/w01_43.js
@@ -4,6 +4,9 @@ const l = console.log
 const $lib = {
   userInput: $('.input-number'),
   addBtn: $('.btn-add'),
+  // subtractBtn: $('.btn-subtract'),
+  // multiplyBtn: $('.btn-multiply'),
+  divideBtn: $('.btn-divide'),
   currentResultOutput: $('.current-result'),
   currentCalculationOutput: $('.current-calculation'),
 }
@@ -15,7 +18,7 @@ const res = {
 
 for (const k in $lib) l(k, $lib[k])
 
-const getUserInput = () => parseInt($lib.userInput.value)
+const getUserInput = () => +$lib.userInput.value
 
 const createOperator = (cb) => () => {
   const op1 = res.current
@@ -23,10 +26,14 @@ const createOperator = (cb) => () => {
   const [result, process] = cb(op1, op2)
   $lib.currentResultOutput.textContent = res.current = result
   $lib.currentCalculationOutput.textContent = process
-  console.log('calText', process)
+  l(`${process} = ${result}`)
 }
 
-$lib.addBtn.addEventListener(
-  'click',
-  createOperator((o1, o2) => [o1 + o2, `${o1} + ${o2}`])
-)
+const eLib = {
+  addBtn: (o1, o2) => [o1 + o2, `${o1} + ${o2}`],
+  // subtractBtn: (o1, o2) => [o1 - o2, `${o1} - ${o2}`],
+  // multiplyBtn: (o1, o2) => [(o1 * o2).toFixed(2), `${o1} * ${o2}`],
+  divideBtn: (o1, o2) => (o2 ? [(o1 / o2).toFixed(2), `${o1} / ${o2}`] : [o1, 'cannot divide by 0']),
+}
+
+for (const k in eLib) $lib[k].addEventListener('click', createOperator(eLib[k]))

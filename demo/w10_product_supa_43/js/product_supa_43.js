56e4abb671664a8ba1f0dd5dca5d2cde5ff42274
victor_xu <pure90719@gmail.com>
2025-04-27 18:19:05 +0800

    W10-P1: Copy mid1 m3_43 answer and rearrange it for W10 demo

---
+++ demo/w10_product_supa_43/js/product_supa_43.js
@@ -0,0 +1,42 @@
+import { _supabase } from './clientSupabase.js';
+
+const productContainer = document.querySelector('.products-container');
+
+let products_43 = [];
+
+const fetchProducts = async () => {
+  try {
+    let { data, error } = await _supabase.from('product_43').select('*');
+    console.log('data', data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const displayProducts = (products) => {
+  let productsContent = products
+    .map((product) => {
+      const { id, title, price, remote_img } = product;
+      return `
+        <div class="single-product">
+        <img
+          src=${remote_img}
+          class="single-product-img img"
+          alt=${title}
+        />
+        <footer>
+          <h3 class="name">${id}-${title}</h3>
+          <span class="price">$${price}</span>
+        </footer>
+      </div>
+    `;
+    })
+    .join('');
+  productContainer.innerHTML = productsContent;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  products_43 = await fetchProducts();
+  displayProducts(products_43);
+});
+
